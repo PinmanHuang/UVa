be216e146d0e5a81062746efb3b8304ca68674c5
Huang pin-man <pinmanhuang@gmail.com>
2019-04-20 16:45:56 +0800

    272 done

---
+++ 272/272.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    string str;
+    bool isFirst = true, firstLine = true;
+    while (getline(cin, str)) {
+        // Check if it is the first line, if flase then print the newline for previous output.
+        if (!firstLine) {
+            cout << '\n';
+        }
+        // Find the " and check if it is the first show, if true then replacing with ``, otherwise, replacing with ''.
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i]=='"') {
+                if (isFirst) {
+                    cout << "``";
+                    isFirst = false;
+                }  else {
+                    cout << "''";
+                    isFirst = true;
+                }
+            } else {
+                cout << str[i];
+            }
+        }
+        firstLine = false;
+    }
+    return 0;
+}
